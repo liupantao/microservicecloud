119b19c80635703bad7ee1975fa42b391a917ddf
liupantao <804878782@qq.com>
2019-02-27 15:06:27 +0800

    分支测试备注2

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/Application.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/Application.java
@@ -11,7 +11,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application
 {
     public static void main( String[] args )
+
     {
+        System.out.println("--分支测试备注2--123");
         SpringApplication.run(Application.class,args);
     }
 }
