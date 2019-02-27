5f03a7026f43138be975da5db33e10c4cea512a2
liupantao <804878782@qq.com>
2019-02-27 18:30:49 +0800

    增加Eureka模块，并把dept服务注册到Eureka

--- microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/App.java
+++ microservicecloud-provider-dept-8001/src/main/java/com/atguigu/springcloud/App.java
@@ -5,12 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
- * Hello world!
+ * microservicecloud-provider-dept-8001
  *
  */
 @SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
 public class App
 {
     public static void main( String[] args )
