fbe0ecec1ccbfafd2d8e203f50f7e7b001cac722
liupantao <804878782@qq.com>
2019-02-28 16:23:27 +0800

    Eureka集群搭建 7001,7002,,7003

---
+++ microservicecloud-eureka-7002/src/main/java/com/atguigu/springcloud/Application.java
@@ -0,0 +1,21 @@
+package com.atguigu.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableEurekaServer//EurekaServer服务器端启动类,接受其它微服务注册进来
+public class Application
+{
+    public static void main( String[] args )
+
+    {
+
+        SpringApplication.run(Application.class,args);
+    }
+}
