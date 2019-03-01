4ed4fbe271c6a3d96e16c529461c3defd3ddba58
liupantao <804878782@qq.com>
2019-03-01 18:32:40 +0800

    1

---
+++ microservicecloud-provider-dept-8001-hystrix/src/main/java/com/atguigu/springcloud/App.java
@@ -0,0 +1,27 @@
+package com.atguigu.springcloud;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * microservicecloud-provider-dept-8001
+ *
+ */
+@SpringBootApplication
+@EnableEurekaClient //本服务启动后会自动注册进eureka服务中
+@EnableDiscoveryClient //开启服务发现
+@EnableCircuitBreaker//对hystrixR熔断机制的支持
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello ------ World!" );
+        SpringApplication.run(App.class, args);
+    }
+}
